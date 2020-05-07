dc3c54e49bf22f6ebb5e9a94d5a94bf4665d7362
strus38 <indigoping4cgmi@gmail.com>
2020-05-07 17:18:30 +0200

    fix

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,8 +4,8 @@ if [ -z ${SLURM_CLUSTER_NAME+x} ]; then echo "SLURM_CLUSTER_NAME not set" && exi
 if [ -z ${SLURM_CONTROL_MACHINE+x} ]; then echo "SLURM_CONTROL_MACHINE not set" && exit 1; fi
 if [ -z ${SLURM_NODE_NAMES+x} ]; then echo "SLURM_NODE_NAMES not set" && exit 1; fi
 
-sed -i -e "s/###SLURM_CLUSTER_NAME###/${SLURM_CLUSTER_NAME}/g" /usr/local/etc/slurm.conf
-sed -i -e "s/###SLURM_CONTROL_MACHINE###/${SLURM_CONTROL_MACHINE}/g" /usr/local/etc/slurm.conf
-sed -i -e "s/###SLURM_NODE_NAMES###/${SLURM_NODE_NAMES}/g" /usr/local/etc/slurm.conf
+sed -i -e "s/###SLURM_CLUSTER_NAME###/${SLURM_CLUSTER_NAME}/g" /usr/local/etc/slurm/slurm.conf
+sed -i -e "s/###SLURM_CONTROL_MACHINE###/${SLURM_CONTROL_MACHINE}/g" /usr/local/etc/slurm/slurm.conf
+sed -i -e "s/###SLURM_NODE_NAMES###/${SLURM_NODE_NAMES}/g" /usr/local/etc/slurm/slurm.conf
 
 /usr/bin/supervisord --nodaemon
